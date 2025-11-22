d45537d717d0acbad02aa2a46fc15692613baf55
SirAxLord <A325641@alumnos.uaslp.mx>
2025-11-22 11:42:49 -0600

    Versione las routas de la api, a version 1

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
-<?php 
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\ServiceController;
 
-Route::apiResource('products', ProductController::class);
-Route::apiResource('services', ServiceController::class);
+Route::prefix('v1')->group(function () {
+	Route::apiResource('products', ProductController::class);
+	Route::apiResource('services', ServiceController::class);
+});
